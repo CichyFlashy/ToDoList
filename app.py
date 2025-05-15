e00ee7e7d80fa5d0a5f77f5198402091645150c9
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-05-15 15:44:07 +0200

    feat: Add endpoint for getting all tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import datetime
 
 app = Flask(__name__)
@@ -10,7 +10,19 @@ class ToDo:
         self.description = description
         self.date = datetime.now()
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "date": self.date
+        }
 
+todos = []
+
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    return jsonify([todo.to_dict for todo in todos]) 
 
 if __name__ == "__main__":
     app.run()
